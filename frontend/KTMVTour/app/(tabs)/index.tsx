028570d05fd2121b2fe7b6ae929e09b35fca69d6
Ashutosh Kuinkel <108271619+hejjwheh@users.noreply.github.com>
2025-11-23 09:29:16 +1100

    rn video installation

--- frontend/KTMVTour/app/(tabs)/index.tsx
+++ frontend/KTMVTour/app/(tabs)/index.tsx
@@ -5,6 +5,7 @@ import InteractiveMapCard from "../../components/landing/interactiveMap-card";
 import FeaturedPosts from "../../components/landing/featured-posts";
 import ShareExperienceCard from "../../components/landing/share-experience.card";
 import CategoryCards from "../../components/landing/historical&temples.cards";
+import Video from "react-native-video" 
 
 export default function Index() {
   return (
@@ -15,20 +16,15 @@ export default function Index() {
       scrollEventThrottle={16}
     >
       <View className="flex-1 items-center pt-10 pb-12 bg-black min-h-screen">
+        <Video source={require('../../assets/vid/Comp1.mp4')}/>
         {/* Page heading */}
         <Heading />
 
         {/* Take me to landmark card */}
         <TakeToLandmark />
 
-        {/* Interactive map card */}
-        <InteractiveMapCard />
       </View>
 
-      <View className="flex-1 bg-black pb-8">
-        {/* Check what's hot today posts */}
-        <FeaturedPosts />
-      </View>
 
       <View className="flex-1 items-center bg-black pb-8">
         {/* Share your experience {post/checkIn} card */}
